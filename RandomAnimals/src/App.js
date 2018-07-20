032d466444477b04fe7f790a7badadc8552549f7
phdnguyen <phongdnguyen17@gmail.com>
2018-07-20 15:37:45 +0700

    Polished UI a bit

--- RandomAnimals/src/App.js
+++ RandomAnimals/src/App.js
@@ -21,27 +21,29 @@ class App extends Component {
 
   componentDidMount() {
 
-    axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5714c28b5fe74b12ae85d0833e4863e6').then(res => this.setState({ data: res.data.articles[1], loading: false }))
+    axios.get('https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=5714c28b5fe74b12ae85d0833e4863e6').then(res => this.setState({ data: res.data.articles[0], loading: false }))
 
   }
 
 
   render() {
     return (
-      <View style={{ flex: 1 , alignContent: 'center'}}>
-        <View style = {{height: 50}}></View>
-        <ScaledImage uri =  'https://vignette.wikia.nocookie.net/logopedia/images/a/ad/Headline_NEWS_logo.png/revision/latest?cb=20151226141200' width = {Dimensions.get('screen').width}/>
-        <View style ={{height: 100}}></View>
-        <Text style = {styles.text}>{this.state.data.title}</Text>
-        <View style = {{height: 50}}></View>
-        <Text style = {styles.text}>{this.state.data.description}</Text>
-        <View style = {{height: 50}}></View>
-        <Text style = {styles.text}>{this.state.data.author}</Text>
-        <View style = {{height: 50}}></View>
-        <TouchableOpacity style = {{flex: 1}} onPress={ ()=> Linking.openURL(this.state.data.url) }>
-        <Text style = {styles.text}>{this.state.data.url}</Text>
+      <View style={{ flex: 2, padding: 10 }}>
+        <View style={{ height: 30 }}></View>
+        <View style={{ height: 100, alignContent: 'center', alignItems: 'center' }}>
+          <Image source={require('./logo.jpg')} style={{ height: '40%', width: '40%' }} />
+        </View>
+        <Text style={styles.textTitle}>{this.state.data.title}</Text>
+        <View style={{ height: 50 }}></View>
+        <Text style={styles.text}>{this.state.data.description}</Text>
+        <View style={{ height: 20 }}></View>
+        <View style={{ alignContent: 'center', alignItems: 'center' }}>
+          <Image source={{ uri: this.state.data.urlToImage }} style={{ height: 200, width: 200 }} />
+        </View>
+        <View style={{ height: 20 }}></View>
+        <TouchableOpacity style={{}} onPress={() => Linking.openURL(this.state.data.url)}>
+          <Text style={styles.textLink}>Click here to view full article</Text>
         </TouchableOpacity>
-        {/* <ScaledImage uri = {this.state.data.urlToImage} width = {Dimensions.get('screen').width}/> */}
       </View>
     );
   }
@@ -49,9 +51,24 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   text: {
-    fontFamily: 'BodoniSvtyTwoSCITCTT-Book',
-    fontSize: 20
-  }
+    fontFamily: 'American Typewriter',
+    fontSize: 18
+  },
+  textTitle: {
+    fontFamily: 'American Typewriter',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center'
+  },
+
+  textLink: {
+    fontFamily: 'American Typewriter',
+    fontSize: 18,
+    textAlign: 'center'
+
+  },
+
+
 })
 
 export default App;
